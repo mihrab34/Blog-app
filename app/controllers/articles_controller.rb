16f0aca7bac26e05baafa81a620078341a57a2b5
Akintola Ramota <akintolarahmah@gmail.com>
2020-05-05 00:35:24 +0000

    styles, update and destoy methods added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,14 +10,44 @@ class ArticlesController < ApplicationController
     
 
     def new
+        @article = Article.new
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
     end
     
     def create
         @article = Article.new(article_params)
         
-        @article.save
-        redirect_to @article
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
     end
+
+    def update
+        @article = Article.find(params[:id])
+        
+        if @article.update(article_params)
+           redirect_to @article
+        else 
+           render 'edit'
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        if @article.destroy         
+            flash[:success] = 'Post was successfully deleted.'
+            redirect_to articles_path
+        else
+            flash[:error] = 'Something went wrong'
+            redirect_to articles_path
+        end
+    end
+    
 
     private
     def article_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :articles,  only: [:edit, :update, :new, :create, :show]
+  resources :articles
   root 'articles#index'
 
 end
