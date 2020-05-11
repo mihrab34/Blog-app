d146071a9d050d61b4e7bf1bab2ee956a3f22a95
Akintola Ramota <akintolarahmah@gmail.com>
2020-05-11 22:13:43 +0000

    authentication added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+    http_basic_authenticate_with name: "lola", password: "secret", except: [:destroy]
+
     def create
         @article = Article.find(params[:article_id])
         @comment = @article.comments.create(comment_params)
